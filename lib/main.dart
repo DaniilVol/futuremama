d7330a579c1ef5a0f7f9af7e3e520d5747d884c0
daniilvol <theytka@gmail.com>
2024-02-09 14:03:35 +0300

    первый коммит

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const FuturemamaApp());
+  runApp(FuturemamaApp());
+}
+
+class FuturemamaApp extends StatefulWidget {
+  const FuturemamaApp({super.key});
+
+  @override
+  State<FuturemamaApp> createState() => _FuturemamaAppState();
+}
+
+class _FuturemamaAppState extends State<FuturemamaApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
